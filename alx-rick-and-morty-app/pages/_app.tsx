7fc3c9d5eca4e2da04076a0d0fad9367927804b6
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 <jonmersha@gmail.com>
2025-07-01 00:54:48 +0300

    my app

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -2,12 +2,22 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/apolloClient";
-import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorBoundary from "@/components/ErrorBoundary";
 
-export default function App({ Component, pageProps }: AppProps) {
+export  function App({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary >
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
       <Component {...pageProps} />
     </ErrorBoundary>
   );
 }
+
+export default MyApp;
